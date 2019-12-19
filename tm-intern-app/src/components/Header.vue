b3ab1773be46a50b84d160de29c217d63ed9e062
t_iwase <iwase2248@gmail.com>
2019-12-19 16:31:42 +0900

    ちょっと変更、まだ全然触ってない

---
+++ tm-intern-app/src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="header">
+    <p class="title">テクノモバイル 新卒採用Apps</p>
+    <div class="menu">
+      <p>menu | menu | menu | menu</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header{
+  color: white;
+  font-family: arial black;
+  position: absolute;
+  left:0;
+  top:0;
+  width: 100%;
+  background:#000;
+}
+
+.title{
+  text-align: left;
+}
+
+.menu{
+  text-align: right;
+}
+
+</style>
